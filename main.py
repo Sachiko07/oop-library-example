5156c76382128735036c2dbb426eb05b2e93ad1e
Sachiko07 <95449474+Sachiko07@users.noreply.github.com>
2025-01-23 05:41:28 +0000

    Library & Book Info

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 class Library:
-    pass
+    name = "Tinashe's Library"
+    address = "36 Lansville Road"
+    book = ["OOP Project", "Divergent", "Harry Potter"]
 
 class Book:
-    pass
+    title = "OOP Project"
+    author = "Tinashe Murungweni"
+    id_number "34676"
+    category = "Information & Technology"
+    price = 8.99
+    
 
     
